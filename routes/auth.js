804b202228f673c3e54a7aead00a9a5a40ff43c8
thanatos9520 <jose.molina.cascante@gmail.com>
2022-01-10 00:07:39 -0600

    Se agregan routes

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
-const { login, googleSignin  } = require('../controllers/auth');
+const { login, googleSignin } = require('../controllers/auth');
 
 
 const router = Router();
@@ -20,8 +20,6 @@ router.post('/google',[
     check('id_token', 'El id_token es necesario').not().isEmpty(),
     validarCampos
 ], googleSignin );
-
-
 
 
 
